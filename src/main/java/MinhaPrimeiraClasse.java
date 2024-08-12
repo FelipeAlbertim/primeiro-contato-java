e949644c06b40e00a323629b455a518ccb1efc4f
FelipeAlbertim <felipe.albertim@sptech.school>
2024-08-12 08:20:21 -0300

    Teste o versionamento para a aula de Linguagem de Programação !

--- src/main/java/MinhaPrimeiraClasse.java
+++ src/main/java/MinhaPrimeiraClasse.java
@@ -1,5 +1,6 @@
 public class MinhaPrimeiraClasse {
     public static void main(String[] args) {
         System.out.println("Olá Mundo !");
+        System.out.println("Teste para a aula de LP !!");
     }
 }
